2d488bd400641316685886117e023882062a45ed
FoxViki <white-kitten-63@mail.ru>
2023-08-05 00:19:45 +0400

    zad6

--- task06/Program.cs
+++ task06/Program.cs
@@ -1,3 +1,13 @@
 ﻿/*напишите прагму, кот на вход принимает число и выдает,
 является ли число четным
 */
+Console.WriteLine("Введи число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n%2 == 0)
+{
+    Console.WriteLine("Число{n} - четное");
+}
+else(n%2 == 1)
+{
+    Console.WriteLine("Число{n} - нечетное");
+}
