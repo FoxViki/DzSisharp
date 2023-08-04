c5c62781b3ee38ba632f84f60364c50a4b30e260
FoxViki <white-kitten-63@mail.ru>
2023-08-04 23:56:09 +0400

    zad4

--- task04/Program.cs
+++ task04/Program.cs
@@ -1,3 +1,14 @@
 ﻿/* Напишите прагму, кот принимает на вход 3 числа
 и выдает максимальное из этих чисел
 */
+Console.WriteLine("Введи первое числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max =c;
+
+Console.WriteLine(max);
