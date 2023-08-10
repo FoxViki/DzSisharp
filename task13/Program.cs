2e9144b471ae24d37c5f8c2eedf3c5ceab689d09
FoxViki <white-kitten-63@mail.ru>
2023-08-10 16:05:53 +0400

    ggg

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру
+заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int number = Console.WriteLine("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numberOne = number / 100;
+Console.WriteLine(numberOne);
