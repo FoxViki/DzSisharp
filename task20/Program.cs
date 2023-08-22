eddecc0f701c2c749122e0560c89dea4add1f087
FoxViki <white-kitten-63@mail.ru>
2023-08-22 12:11:57 +0400

    jdjddj

---
+++ task20/Program.cs
@@ -0,0 +1,26 @@
+﻿/* 20. Напишите программу, которая принимает на
+вход координаты двух точек и находит расстояние между ними
+в 2D пространстве. */
+
+System.Console.WriteLine("Введите координаты первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координаты второй точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+double S = 0;
+S = Math.Sqrt(Math.Pow(x2-x1,2)+Math.pow(y2-y1,2));
+Console.Write("Расстояние между точками:" + Math.Round(S,2));
+
+ИЛИ МОЖНО ТАК
+
+System.Console.WriteLine("Введите координаты первой точки");
+int userx1 = Convert.ToInt32(Console.ReadLine());
+int usery1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координаты второй точки");
+int userx2 = Convert.ToInt32(Console.ReadLine());
+int usery2 = Convert.ToInt32(Console.ReadLine());
+double S = 0;
+S = Math.Sqrt(Math.Pow(userx2-userx1,2) + Math.Pow(usery2-usery1,2) );
+Console.Write("Расстояние между двумя точками:" + Math.Round(S,2));
