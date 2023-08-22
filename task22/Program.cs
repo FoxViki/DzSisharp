94b56c9fc44d9454a9cb12c0732cf6f54d01b972
FoxViki <white-kitten-63@mail.ru>
2023-08-22 14:03:22 +0400

    jjjd

---
+++ task22/Program.cs
@@ -0,0 +1,11 @@
+﻿/* 22. Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу квадратов чисел от 1 до N.
+*/
+
+Console.WriteLine("Введи число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i <= numberN; i = i + 2)
+{
+    int result = i*i;
+    Console.Write(result);
+}
