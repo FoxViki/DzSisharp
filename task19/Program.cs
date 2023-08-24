96e3c49de6f6b2569ebd1f43bd871164e00ef7f0
FoxViki <white-kitten-63@mail.ru>
2023-08-24 16:44:17 +0400

    hh

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,21 +1,21 @@
-﻿Console.WriteLine("Введите число: ") 
-int number1 = Convert.ToInt32(Console.ReadLine());
-int a = Math.Round(number1/10000);
-int b = Math.Round(number1/1000) - a*10;
-int c = Math.Round(number1/100) - (a*100+b*10);
-int d = Math.Round(number1/10) - (a*1000+b*100+c*10);
-int e = number1 - (a*10000+b*1000+c*100+d*10);
-int temp = 0;
-int temp1 = 0;
-temp = a;
-int a = e;
-int e = temp;
-temp1 = b;
-int b = d;
-int d = temp1;
-int number2 = a*10000+b*1000+c*100+d*10+e;
-Console.WriteLine(number2);
-if (number1 == number2)
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и
+проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = (number/10000);
+int b = (number/1000) - a*10;
+int c = (number/100) - (a*100+b*10);
+int d = (number/10) - (a*1000+b*100+c*10);
+int e = number - (a*10000+b*1000+c*100+d*10);
+
+int number1 = e*10000+d*1000+c*100+b*10+a;
+Console.WriteLine(number1);
+if (number == number1)
 {
     Console.WriteLine($"Число {number1} является палиндромом");
 }
