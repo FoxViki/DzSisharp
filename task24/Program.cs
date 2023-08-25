148bf9908a84a336870391ea5ab5391ceb8bc345
FoxViki <white-kitten-63@mail.ru>
2023-08-25 18:09:15 +0400

    jfjf

---
+++ task24/Program.cs
@@ -0,0 +1,32 @@
+﻿/* напишите программу, которая на вход принимает число и выдает сумму цифр от 1 до а.
+*/
+
+/*
+
+Console.WriteLine("Введи число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+for (int i = 1; i <= userNumber; i++)
+{
+    sum += i;
+}
+Console.WriteLine(sum);
+
+
+
+ИЛИ МОЖНО ТАК */
+
+Console.WriteLine("Введи число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int SumOfNumber(int number);
+{
+    int sum = 0;
+    for (int i = 1; i <= userNumber; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+Console.WriteLine($"сумма чисел в числе {userNumber} равна {SumOfNumber(userNumber)}");
---
+++ task26/Program.cs
@@ -0,0 +1,17 @@
+﻿/*напишите программу, которая на вход принимает число,
+а на выходе считает количество цифр в числе.
+*/
+
+Console.WriteLine("Введи число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int Quantity(int number)
+{
+    int count = 0;
+    while (number >0)
+    {
+        count++;
+        number = number/10;
+    }
+    return count;
+};
