339d0fedcef7bd035c2d8089759de227400d9a97
FoxViki <white-kitten-63@mail.ru>
2023-08-31 19:35:42 +0400

    sss

--- task38/Program.cs
+++ task38/Program.cs
@@ -2,78 +2,43 @@
 Найдите разницу между максимальным и минимальным элементов массива.
 [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76*/
 
+Console.Write("Введите количество элементов массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine("Введи число: ");
-int arrayL = Convert.ToInt32(Console.ReadLine());
-double minim = Convert.ToInt32(Console.ReadLine());
-double maxim = Convert.ToInt32(Console.ReadLine());
 
-void ControlMinMax(double mininal, double maximal)
+double[] GetArray (int arrayL)
 {
-    if (minimal < 0 && maximal < 0)
-    {
-        Console.WriteLine("Введите корректное число");
-    }
+double[] array = new double[arrayL];
+for (int i = 0; i < arrayL; i++)
+{
+array[i] = new Random().NextDouble();
+Console.Write($"{array[i]:F2} {" "}");
+}
+return array;
 }
 
 
-double[] GetRandomArray(int arrayLength1, double start, double end)
+
+
+
+double Raz(double[] array)
 {
-    double[] array = new double[arrayLength1];
-    for (int i=0; i<array.Length; i++)
-    {
-        array [i] = new Random().Next(start, end);
-    }
-    return array;
+double min = array[0];
+double max = array[0];
+int i = 1;
+while (i < array.Length)
+{
+if (max<array[i])
+max = array[i];
+if (min>array[i])
+min = array[i];
+i = i + 1;
+}
+return max-min;
 }
 
-void PrintArray(double [] arrayToPrint)
-{
-    Console.Write("[");
-    for (int i=0; i<arrayToPrint.Length; i++)
-    {
-       Console.Write(arrayToPrint[i]);
-       if (i<arrayToPrint.Length - 1)
-       {
-        Console.Write (", ");
-       }
-    }
-    Console.Write("]");
-}
 
-double MinMaxNumber(double[] array)
-{
-    double min = array[0];
-    double max = array[0];
-    int i = 0;
-    
-    for(int i = 1; i < array.Length; i++)
-    {
-        if (array[i] > max)
-        {
-            max = array[i];
-        }
-        if (array[i]< min)
-        {
-            min = array[i];
-        }
-    }
-    return min;
-    return max;
-    Console.WriteLine($" {double min} = {min}");
-    Console.WriteLine($" {double max} = {max}");
-}
+double[] userArray = GetArray(length);
 
-double Raznost(userArray)
-{
-    double razn = max - min;
-    return razn;
-}
-
-ControlMinMax(minim, maxim);
-double[] userArray = GetRandomArray(arrayL, minim, maxim);
-PrintArray(userArray);
-MinMaxNumber(userArray);
-double raz = Raznost(min, max);
 Console.WriteLine();
-Console.WriteLine(sumNPoz);
+Console.Write($"Разница между максимальным и минимальным элементов массива: {Raz(userArray):F2}");
