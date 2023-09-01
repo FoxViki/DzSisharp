08e5f0d00685b6b147928806cbd1a329211dd0c4
FoxViki <white-kitten-63@mail.ru>
2023-09-01 17:55:46 +0400

    jjj

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+
+Console.Clear();
+
+Console.WriteLine("Введите длину массива: ");
+int arrayL = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [arrayL];
+
+
+int[] ArrayL(int[] array)
+{
+    int[] arrayL = new int [array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите число массива: ");
+        arrayL[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arrayL;
+};
+
+void PrintArray(int [] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i=0; i<arrayToPrint.Length; i++)
+    {
+       Console.Write(arrayToPrint[i]);
+       if (i<arrayToPrint.Length - 1)
+       {
+        Console.Write (", ");
+       }
+    }
+    Console.Write("]");
+};
+
+
+int QuantityNumber(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 0)
+        {
+            count = count +1;
+        }
+    }
+    return count;
+};
+
+int[] userArray = ArrayL(array);
+
+PrintArray(userArray);
+
+Console.WriteLine();
+Console.WriteLine($"Количество положительных чисел = {QuantityNumber(userArray)}");
--- task43/Program.cs
+++ task43/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Введите числа: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double SearchX(double b1, double k1, double b2, double k2)
+{
+    /*y == k1 * x + b1;
+    y == k2 * x + b2;
+    k1 * x +b1 == k2 * x +b2;
+    k1*x - k2*x == b2 - b1;
+    (k1-k2)*x==b2-b1;*/
+    double x = (b2-b1)/(k1-k2);
+    return x;
+};
+
+double xP = SearchX(b1, k1, b2, k2);
+
+double SearchY(double x, double b1, double k1)
+{
+    double y = (k1 * x) + b1;
+    return y;
+};
+
+double yP = SearchY(xP, b1, k1);
+
+Console.WriteLine();
+Console.Write(xP);
+Console.WriteLine();
+Console.Write(yP);
+Console.WriteLine();
+Console.WriteLine($"(Точка пересечения имеет координаты [{xP}, {yP}])");
