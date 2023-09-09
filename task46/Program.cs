1cd31913f14755b2bf43031ef13c98de2d9e3197
FoxViki <white-kitten-63@mail.ru>
2023-09-09 12:24:59 +0400

    jj

--- task46/Program.cs
+++ task46/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задайте двумерный массив размером m*n, заполненный
+случайными целыми числами.*/
+
+Console.Clear();
+
+int[,] Get2DArray(int row, int col, int deviation)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = new Random().Next(-deviation, deviation);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+     System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(arrayToPrint[i,j] +"\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+int[,] randomArray = GetRandom2DArray(5, 5, 10);
+Print2DArray(randomArray);
+
+int [,] arr = Get2DArray(5,5,10);
+Print2DArray(arr);
