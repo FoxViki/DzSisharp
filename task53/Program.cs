36f1f07b4caf19acd6a5361d48000dbfebc97785
FoxViki <white-kitten-63@mail.ru>
2023-09-11 11:16:56 +0400

    kk

--- task53/Program.cs
+++ task53/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* задайте двумерный массив. напишите прогу кот поменяет местами
+первую и последнюю строку массива
+*/
+
+int[,] Get2DArray(int rows, int cols, int min, int max)
+{
+    int[,] array = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] ResultArray (int [,] array)
+{
+int rows = array.GetLength(0);
+int cols = array.GetLength(0);
+int temp = 0;
+for (int i = 0; i < cols, i++)
+{
+    temp = array[0, i];
+    array [0, i] = array [rows-1, i];
+    array [rows-1, i] = temp;
+}
+return array; 
+}
+
+int[,] userArray = Get2DArray(4, 4, 0, 10);
+Print2DArray(userArray);
+Console.WriteLine();
+int[,] userArray2 = ResultArray(userArray);
+Print2DArray(userArray2);
--- task55/Program.cs
+++ task55/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* задайте двумерный массив. напишите прогу кот поменяет местами
+столбец на строку массива*/
+
+
+int[,] Get2DArray(int rows, int cols, int min, int max)
+{
+    int[,] array = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeStringsAndColumns(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = i; j < matrix.GetLength(1); i++)
+        {
+            temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+    return matrix
+}
+
+int[,] userArray = Get2DArray(5, 5, 0, 10); ///действует только для квадратов. поэтому колво строк и столбов одинаковое
+Print2DArray(userArray);
+Console.WriteLine();
+Print2DArray(ChangeStringsAndColumns(userArray));
