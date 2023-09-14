af870348cd357bff84c71685c9c0fceee2c496c1
FoxViki <white-kitten-63@mail.ru>
2023-09-14 11:17:19 +0400

    jjj

--- task63/Program.cs
+++ task63/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿/*63. Задайте значение N. Напишите программу, которая выведет все натуральные
+числа в промежутке от 1 до N.
+*/
+
+
+
+void NuturalNum(int n, int start = 1)
+{
+if (start <= n)
+{
+System.Console.Write(start+", ");
+NuturalNum(n, start + 1);
+}
+}
+NuturalNum(5);
+
+
+
+
+и еще
+
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string numbers = string.Empty;
+
+void Enumeration(int a)
+{
+if (a == 1) numbers = "1 " + numbers;
+else
+{
+numbers = Convert.ToString(a) + " " + numbers;
+Enumeration(a - 1);
+}
+}
+Enumeration(n);
+Console.WriteLine(numbers);
+
+
--- task65/Program.cs
+++ task65/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 65. Задайте значения M и N. Напишите программу, которая выведет
+все натуральные числа в промежутке от M до N.*/
+
+void NuturalNum(int m, int n)
+{
+if (m <= n)
+{
+Console.Write(m + ", ");
+NuturalNum(m + 1 , n);
+}
+}
+NuturalNum(1,7);
