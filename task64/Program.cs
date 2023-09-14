7d2d937e54e608d4295c08de4320cf6a1b854ae1
FoxViki <white-kitten-63@mail.ru>
2023-09-14 13:34:22 +0400

    hhh

--- task64/Program.cs
+++ task64/Program.cs
@@ -3,3 +3,15 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
+Console.WriteLine("Введите начало отсчета: ");
+int start = Convert.ToIn32(Console.ReadLine());
+
+void NuturalNum(int finish = 1, int start)
+{
+if (start >= finish)
+{
+Console.Write(start +", ");
+NuturalNum(finish, start - 1);
+}
+}
+NuturalNum(int start);
--- task66/Program.cs
+++ task66/Program.cs
@@ -3,3 +3,18 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+
+Console.WriteLine("Введите начало отсчета: ");
+int start = Convert.ToIn32(Console.ReadLine());
+Console.WriteLine("Введите конец отсчета: ");
+int finish = Convert.ToIn32(Console.ReadLine());
+
+int SumNum(int start, int finish,)
+{
+if (start == 0 && finish == 0) return 0;
+else if(start == finish) return finish;
+else if(start <= finish) return start  + SumNum(finish, start + 1);
+else if (start >= finish) return start  + SumNum(finish, start - 1);
+}
+int result = SumNum(int start, int finish);
+
